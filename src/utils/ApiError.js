5d08f0d6ea5160a0c4d919d03600dfa740178b1a
SOURYA DEY <souryadey031@gmail.com>
2024-05-23 11:01:00 +0530

    middlewares

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,23 +1,21 @@
-class ApiError extends Error{
-    constructor (
-        statusCode,
-        message="Something went wrong",
-        errors=[],
-        statck=""
-    ){
-        super(message)
-        this.statusCode=statusCode
-        this.data=null
-        this.message=message
-        this.success=false
-    this.errors=errors
-    if(statck)
-        {
-            this.stack=statck
-        }
-        else{
-            Error.captureStackTrace(this,this.constructor)
-        }
+class ApiError extends Error {
+  constructor(
+    statusCode,
+    message = "Something went wrong",
+    errors = [],
+    stack = ""
+  ) {
+    super(message);
+    this.statusCode = statusCode;
+    this.data = null;
+    this.message = message;
+    this.success = false;
+    this.errors = errors;
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
     }
+  }
 }
-export {ApiError}
+export { ApiError };
